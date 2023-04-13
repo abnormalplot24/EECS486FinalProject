dea9fe7187680240d1d3ecc5440118bc6b826fc7
jonathanli <12lijonathan@gmail.com>
2023-04-12 20:51:09 -0400

    debugging for decision tree

--- Decision Tree (ID3 Algorithm)/decision_tree.py
+++ Decision Tree (ID3 Algorithm)/decision_tree.py
@@ -1,44 +1,10 @@
 import pandas as pd
 import numpy as np
 import math
-import nltk
-import re
 
 # Load the data from the CSV files
 train_df = pd.read_csv('Data/steam_reviews_training.csv')
 test_df = pd.read_csv('Data/steam_reviews_testing.csv')
-
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're","you've", 
-             "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he','him', 'his', 'himself', 
-             'she', "she's", 'her', 'hers', 'herself', 'it', "it's",'its', 'itself', 'they', 'them', 'their', 
-             'theirs', 'themselves', 'what', 'which','who', 'whom', 'this', 'that', "that'll", 'these', 
-             'those', 'am', 'is', 'are','was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 
-             'do','does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because','as', 'until', 
-             'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against','between', 'into', 'through', 'during', 
-             'before', 'after', 'above', 'below','to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 
-             'under', 'again','further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how','all', 
-             'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such','no', 'nor', 'not', 'only', 
-             'own', 'same', 'so', 'than', 'too', 'very', 's','t', 'can', 'will', 'just', 'don', "don't", 'should', 
-             "should've", 'now', 'd','ll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't",
-             'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't",'haven', "haven't", 'isn', 
-             "isn't", 'ma', 'mightn', "mightn't", 'mustn',"mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', 
-             "shouldn't",'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-
-# define a function to perform text preprocessing
-def preprocess_text(text):
-    # convert text to lowercase
-    text = text.lower()
-    # remove numbers
-    text = re.sub(r'\d+', '', text)
-    # remove punctuation
-    text = re.sub(r'[^\w\s]', '', text)
-    # remove custom stopwords
-    text = " ".join(word for word in text.split() if word not in stopwords)
-    return text
-
-# apply the preprocessing function to the "review" column of both dataframes
-train_df['review'] = train_df['review'].apply(preprocess_text)
-test_df['review'] = test_df['review'].apply(preprocess_text)
 
 # Convert 'Recommended' to 1 and 'Not Recommended' to 0
 train_df['label'] = train_df['recommendation'].map({'Recommended': 1, 'Not Recommended': 0})
