c01b78220c4edabc28a01c29587c25786f60efc6
tlhaddad <90931865+tlhaddad@users.noreply.github.com>
2023-04-13 20:19:17 -0400

    Update model.py

--- KNN Experiment 1/model.py
+++ KNN Experiment 1/model.py
@@ -5,8 +5,6 @@ import vectorspace
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import genesis
-nltk.download('genesis')
-nltk.download('wordnet')
 genesis_ic = wn.ic(genesis, False, 0.0)
 from nltk.tokenize import word_tokenize
 from difflib import SequenceMatcher
